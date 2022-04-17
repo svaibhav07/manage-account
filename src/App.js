7688c83e1ca8a71a957b8902fcbef672b896597b
Vaibhav Singh <svaibhavrnc@gmail.com>
2022-04-17 10:57:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [name , setName] = useState('');
+    const [age , setAge] = useState('');
+
+
+const handleNameChange=(e)=>{
+  setName(e.target.value);
+}
+
+const handleAgeChange=(e)=>{
+  setAge(e.target.value);
+}
+
+const handleSubmit=(e)=>
+{
+ console.log('submit');
+  e.preventDefault();
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='Manage-Account'>
+        <form onSubmit={(e)=>{handleSubmit(e)}}>
+        <label >
+          Name:
+        </label><br/>
+        <input type="text" value={name} required onChange={(e) => {handleNameChange(e)}} /><br/>
+        <label>
+          Age:
+        </label><br/>
+        <input typr="text" value={age} required onChange={(e)=>{handleAgeChange(e)}} /><br/>
+        <input type="submit" value="Submit"/>
+        </form>
       </header>
+
     </div>
   );
 }
+
 
 export default App;
